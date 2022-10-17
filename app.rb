36ba1f3320288f51d034d43476b0ed4bb443a586
codertinie <martin.munyao@moringaschool.com>
2022-10-17 11:50:40 +0300

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,8 @@
+print "Pass this test, please."
+puts "Hello World!"
+
+puts [1, 2, 3].inspect
+
+p [1, 2, 3]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner"}]
